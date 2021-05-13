2ff10f88ee12c52fe17ec3f9120413cdaa9963c8
Marin Salinas <marin.salinas@digitalonus.com>
2021-05-13 19:51:18 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,8 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/MARIN-training/s3-bucket/aws"
+  version = "1.15.0"
+  
+  # insert required variables here
+  bucket_prefix = "marin-s3-bucket"
+
+}
